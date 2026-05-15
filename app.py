4667fe67a4e7edf5a2e4f017e337208bcfd93b4f
srinu <srinumudiraj9441@gmail.com>
2026-05-15 13:54:47 +0530

    yaml and config file new

---
+++ app.py
@@ -0,0 +1,11 @@
+name = "Srinu"
+age = 24
+skills = ["Python", "Git", "Linux"]
+
+print("Developer Profile")
+print("------------------")
+print("Name:", name)
+print("Age:", age)
+
+for skill in skills:
+    print("Skill:", skill)
