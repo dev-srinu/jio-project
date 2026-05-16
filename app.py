c85b0bf7496964fcf849e2d645b5387ac19846bb
srinu <srinumudiraj9441@gmail.com>
2026-05-16 23:36:29 +0530

    python code

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-name = "Srinu"
-<<<<<<< HEAD
+name = "Srinu" "mudiraj"
 age = 30
-=======
-age = 28
->>>>>>> 4d3bb5404b60f85043e611faac3035c0557cdd21
 skills = ["Python", "Git", "Linux"]
 
 print("Developer Profile")
